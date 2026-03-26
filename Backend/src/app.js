9b3916b3896730463ceffeb23310d1fa64305cab
JayP2006 <jay91451@gmail.com>
2026-03-26 10:45:06 +0530

    updated backend fix (cors issue etc)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,8 +6,7 @@ const cors=require('cors')
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://ai-powered-code-reviewer-16zd.onrender.com",
-  credentials: true
+  origin: "*"
 }));
 
 app.get('/',(req,res)=>{
